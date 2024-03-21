25e9c9ffec394705fe96f0e81b0aca502eefff1c
Raisa Dorzback <144272001+raisa-d@users.noreply.github.com>
2024-03-21 12:16:48 -0400

    Create isItALetter.py

---
+++ 7kyu/Python/isItALetter.py
@@ -0,0 +1,6 @@
+'''
+Write a funciton which returns true if the string is a letter and false if it is any other type of character (punctuation, numerical, etc.)
+'''
+
+def is_it_letter(s):
+    return s.isalpha()
