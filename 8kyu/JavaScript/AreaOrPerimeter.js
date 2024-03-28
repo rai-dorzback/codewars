95a99dc6aa715249ad30d132093261fc8939a049
Raisa Dorzback <144272001+raisa-d@users.noreply.github.com>
2024-03-28 10:23:48 -0400

    Create AreaOrPerimeter.js

---
+++ 8kyu/JavaScript/AreaOrPerimeter.js
@@ -0,0 +1,23 @@
+/*
+You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+Example(Input1, Input2 --> Output):
+
+6, 10 --> 32
+3, 3 --> 9
+Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
+*/
+
+// METHOD 1
+const areaOrPerimeter = (l, w) => l === w ? l * w : 2*(l+w)
+
+// METHOD 2: if/else statement instead of ternary
+const areaOrPerimeter = (l, w) => {
+  // if its a square, return area
+  if(l === w) {
+    return l * w
+  } else { // if rectangle, return perimeter
+    return (2*l)+(2*w)
+  }
+};
