50c8c860c4dc22bf46a971f42122a0fc413ea488
Raisa Dorzback <144272001+raisa-d@users.noreply.github.com>
2024-04-11 11:02:08 -0400

    Create ExesAndOhs.py

---
+++ 7kyu/Python/ExesAndOhs.py
@@ -0,0 +1,19 @@
+'''
+Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+'''
+
+# METHOD 1
+def xo(s):
+    s = s.lower()
+    return s.count('x') == s.count('o')
+
+# METHOD 2: all on one line using lambda
+xo = lambda s: s.lower().count('x') == s.lower().count('o')
