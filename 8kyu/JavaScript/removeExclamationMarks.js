f03241e676f198841675d1ef97ec95b15dc59c3b
Raisa Dorzback <144272001+raisa-d@users.noreply.github.com>
2024-04-22 11:11:40 -0400

    Update removeExclamationMarks.js

--- 8kyu/JavaScript/removeExclamationMarks.js
+++ 8kyu/JavaScript/removeExclamationMarks.js
@@ -1,4 +1,9 @@
 /*
 Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 */
+
+// method 1: filter
 const removeExclamationMarks = s => s.split('').filter(e => e !== '!').join('');
+
+// method 2: replaceAll
+const removeExclamationMarks = s => s.replaceAll('!', '');
