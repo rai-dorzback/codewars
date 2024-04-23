182b6caa004ce9759dbafac81ddcb09ff8852d56
Raisa Dorzback <144272001+raisa-d@users.noreply.github.com>
2024-04-23 09:01:00 -0400

    Create ghostbusters.py

---
+++ 7kyu/Python/ghostbusters.py
@@ -0,0 +1,16 @@
+'''
+Oh no! Ghosts have reportedly swarmed the city. It's your job to get rid of them and save the day!
+In this kata, strings represent buildings while whitespaces within those strings represent ghosts.
+So what are you waiting for? Return the building(string) without any ghosts(whitespaces)!
+
+Example:
+"Sky scra per" -> "Skyscraper"
+If the building contains no ghosts, return the string:
+"You just wanted my autograph didn't you?"
+'''
+
+def ghostbusters(b):
+    if b.find(' ') == -1:
+        return 'You just wanted my autograph didn\'t you?'
+    else:
+        return b.replace(' ', '')
