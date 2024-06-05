ffed741b79633e2bbf797a8bd2159e6ab15bfa17
Raisa Dorzback <144272001+raisa-d@users.noreply.github.com>
2024-06-05 10:37:46 -0400

    Create summingNumsDigits.js

---
+++ 7kyu/JavaScript/summingNumsDigits.js
@@ -0,0 +1,22 @@
+/*
+Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+
+For example: (Input --> Output)
+
+10 --> 1
+99 --> 18
+-32 --> 5
+Let's assume that all numbers in the input will be integer values.
+*/
+
+const sumDigits = n => {
+  // turn number into array of strings of each digit
+  numArray = String(n).split('');
+  // if the first item in the number array is a negative
+  if (numArray[0] === '-') {
+    // remove negative from array to get absolute values
+    numArray.splice(0, 1)
+  };
+  // use reduce to sum each digit
+  return numArray.reduce((a, b) => +a + +b, 0);
+};
