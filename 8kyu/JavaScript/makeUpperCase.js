0db27c7685b371459a35965d021324a20035167e
Raisa Dorzback <144272001+raisa-d@users.noreply.github.com>
2024-07-10 12:35:24 -0400

    Create makeUpperCase.js

---
+++ 8kyu/JavaScript/makeUpperCase.js
@@ -0,0 +1,40 @@
+/*
+
+*/
+
+// method 1: built in method
+const makeUpperCase = str => str.toUpperCase()
+
+// method 2: roll your own
+let letters = {
+  'a': 'A',
+  'b': 'B',
+  'c': 'C',
+  'd': 'D',
+  'e': 'E',
+  'f': 'F',
+  'g': 'G',
+  'h': 'H',
+  'i': 'I',
+  'j': 'J',
+  'k': 'K',
+  'l': 'L',
+  'm': 'M',
+  'n': 'N',
+  'o': 'O',
+  'p': 'P',
+  'q': 'Q',
+  'r': 'R',
+  's': 'S',
+  't': 'T',
+  'u': 'U',
+  'v': 'V',
+  'w': 'W',
+  'x': 'X',
+  'y': 'Y',
+  'z': 'Z'
+};
+
+function makeUpperCase(str) {
+  return str.split('').map(char => letters[char] || char).join('')
+}
