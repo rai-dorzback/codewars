b2e7de3a54a30f342e7b89a02cb3ae6d88ca8609
Raisa Dorzback <144272001+raisa-d@users.noreply.github.com>
2024-07-11 11:35:39 -0400

    Create squareArgument.js

---
+++ 8kyu/JavaScript/squareArgument.js
@@ -0,0 +1,12 @@
+/*
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+// method 1
+const square = a => a**2
+
+// method 2
+const square = a => a**a
+
+// 3
+const square = a => Math.pow(a, 2)
