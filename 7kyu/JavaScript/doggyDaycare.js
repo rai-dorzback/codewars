8d0ad696ba7eecaf958c8632cd193c3138718aa0
Raisa Dorzback <144272001+raisa-d@users.noreply.github.com>
2024-08-08 11:40:53 -0400

    Create doggyDaycare.js

---
+++ 7kyu/JavaScript/doggyDaycare.js
@@ -0,0 +1,39 @@
+/*
+The owner of the doggy daycare wants to know whether she can accept new dogs into her daycare and needs to check whether they have been wormed and vaccinated in order to accept them either by themselves or as well as other dogs.
+
+There is a Dog prototype (class in Ruby) that needs to be extended with the method checkDog (check_dog). Dog currently has the following properties (attributes):
+
+name
+age
+breed
+vaccinated
+wormed
+You'll need to return the dog's name each time as well as " can be accepted", " can only be accepted by itself" and " can not be accepted".
+
+If the dog is vaccinated and wormed, the dog can be accepted.
+If the dog is only vaccinated or only wormed, the dog can only be accepted by itself.
+If the dog has not be vaccianted or wormed, the dog can not be accepted.
+Example:
+
+var spot = new Dog("Spot", 5, "Labrador", true, false);
+spot.checkDog(); // Should return: "Spot can only be accepted by itself".
+*/
+
+// method 1
+Dog.prototype.checkDog = function() {
+  if(this.vaccinated && this.wormed) {
+    return `${this.name} can be accepted`
+  } else if(this.vaccinated || this.wormed) {
+    return `${this.name} can only be accepted by itself`
+  } else {
+    return `${this.name} can not be accepted`
+  }
+};
+
+// method 2
+Dog.prototype.checkDog = function() {
+  // make possible responses an array
+  const responses = [`${this.name} can be accepted`, `${this.name} can only be accepted by itself`, `${this.name} can not be accepted`]
+  // ternary operator
+  return (this.vaccinated && this.wormed) ? responses[0] : (this.vaccinated || this.wormed) ? responses[1] : responses[2];
+  };
