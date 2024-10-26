2b72bdc35137765f96b228d3b5067933e8f580a8
Raisa Dorzback <raisa.dorzback@gmail.com>
2024-10-26 13:57:39 -0400

    oct 26 challenge

---
+++ 8kyu/JavaScript/basicTraining.js
@@ -0,0 +1,16 @@
+/*
+https://www.codewars.com/kata/511f0fe64ae8683297000001/train/javascript
+
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+
+var websites = [];
+*/
+
+// solution 1
+websites.push("codewars")
+
+// solution 2
+websites[0] = "codewars"
