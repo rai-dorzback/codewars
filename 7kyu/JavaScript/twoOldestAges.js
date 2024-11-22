a5a1ec8cd7756f6452f7f0e90698c47536fa72e4
Raisa Dorzback <raisa.dorzback@gmail.com>
2024-11-22 10:58:42 -0500

    nov 22 challenge

---
+++ 7kyu/JavaScript/twoOldestAges.js
@@ -0,0 +1,26 @@
+/*
+The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age,  oldest age].
+
+The order of the numbers passed in could be any order. The array will always include at least 2 items. If there are two or more oldest age, then return both of them in array format.
+
+For example (Input --> Output):
+
+[1, 2, 10, 8] --> [8, 10]
+[1, 5, 87, 45, 8, 8] --> [45, 87]
+[1, 3, 10, 0]) --> [3, 10]
+*/
+
+// SOLUTION 1
+// arr of nums -> arr of 2 highest values
+function twoOldestAges(arr) {
+    // sort array descending
+    sortedArr = arr.sort((a, b) => b - a);
+    // return 2 oldest (ascending)
+    return [sortedArr[1], sortedArr[0]]
+};
+
+// SOLUTION 2
+const twoOldestAges = arr => arr.sort((a, b) =>  a - b).slice(-2);
+
+console.log(twoOldestAges([1, 2, 10, 8]), [8, 10]);
+console.log(twoOldestAges([1, 5, 87, 45, 8, 8]), [45, 87]);
