2b4f63d382a9e8dd2407f7ad2b03a91a923df377
Raisa Dorzback <144272001+raisa-d@users.noreply.github.com>
2024-12-24 13:44:30 -0500

    Create convertSentencesMyGuy.js

---
+++ 7kyu/JavaScript/convertSentencesMyGuy.js
@@ -0,0 +1,6 @@
+/*
+https://www.codewars.com/kata/59827d0c197a17d4cf000028/train/javascript
+One of the most famous and often used phrases in the lexicon of New Yorkers is "my guy". For this challenge, create a function that adds "my guy" at the end of every sentence.
+*/
+
+const newYorker = str => str.split('.').join(' my guy.')
