eae63d4c9730b101dde24407f2740742ea17f91a
Raisa Dorzback <144272001+raisa-d@users.noreply.github.com>
2025-01-24 11:02:38 -0500

    Update simpleMultiplication.js

--- 8kyu/JavaScript/simpleMultiplication.js
+++ 8kyu/JavaScript/simpleMultiplication.js
@@ -1,4 +1,8 @@
 /*
 This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 */
+// SOLUTION 1
 const simpleMultiplication = n => n % 2 === 0 ? n*8 : n*9;
+
+// SOLUTION 2
+const simpleMultiplication = n => n * (n % 2 === 0 ? 8 : 9);
