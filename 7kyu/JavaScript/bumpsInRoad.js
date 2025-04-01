92fe3dfe97a699a33e4db75a40ab43088ce5632b
Raisa Dorzback <raisa.dorzback@gmail.com>
2025-04-01 14:21:55 -0400

    7kyu

---
+++ 7kyu/JavaScript/bumpsInRoad.js
@@ -0,0 +1,23 @@
+/*
+https://www.codewars.com/kata/57ed30dde7728215300005fa/train/javascript
+Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
+
+Unfortunately for you, your drive is very bumpy! Given a string showing either flat road (_) or bumps (n). If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
+*/
+
+// SOLUTION 1
+// str shows _n
+function bump(str){
+    let numBumps = 0;
+    // count how many bumps are in str
+    for(const x of str) {
+      if (x === 'n') {
+        numBumps++;
+      };
+    };
+    // if bumps <= 15 woohoo else car dead
+    return numBumps <= 15 ? 'Woohoo!' : 'Car Dead';
+};
+
+console.log(bump('n__'), 'Woohoo!');
+console.log(bump('nnnnnnnnnnnnnnnnnnnnn'), 'Car Dead');
