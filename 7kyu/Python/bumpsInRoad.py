3782a8c4c9e9407a9f05717975b66f1e83096521
Raisa Dorzback <raisa.dorzback@gmail.com>
2025-04-05 14:27:25 -0400

    7kyu solution 2

--- 7kyu/Python/bumpsInRoad.py
+++ 7kyu/Python/bumpsInRoad.py
@@ -4,7 +4,10 @@ Your car is old, it breaks easily. The shock absorbers are gone and you think it
 
 Unfortunately for you, your drive is very bumpy! Given a string showing either flat road (_) or bumps (n). If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
 '''
-
+# SOLUTION 1
 def bumps(road):
     bumps = road.count('n')
     return 'Woohoo!' if bumps <= 15 else 'Car Dead'
+
+# SOLUTION 2: lambda function, all one line
+bumps = lambda road : "Woohoo!" if road.count("n") <= 15 else "Car Dead"
