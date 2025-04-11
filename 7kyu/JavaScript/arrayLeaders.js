7eb43b51e38820353c857a50f840b6d1acf015d2
Raisa Dorzback <raisa.dorzback@gmail.com>
2025-04-11 19:52:06 -0400

    7kyu

---
+++ 7kyu/JavaScript/arrayLeaders.js
@@ -0,0 +1,70 @@
+/*
+https://www.codewars.com/kata/5a651865fd56cb55760000e0/solutions/javascript
+Definition
+An element is leader if it is greater than The Sum all the elements to its right side.
+
+Task
+Given an array/list [] of integers , Find all the LEADERS in the array.
+
+Notes
+Array/list size is at least 3 .
+
+Array/list's numbers Will be mixture of positives , negatives and zeros
+
+Repetition of numbers in the array/list could occur.
+
+Returned Array/list should store the leading numbers in the same order in the original array/list .
+
+Input >> Output Examples
+arrayLeaders ({1, 2, 3, 4, 0}) ==> return {4}
+Explanation:
+4 is greater than the sum all the elements to its right side
+
+Note : The last element 0 is equal to right sum of its elements (abstract zero).
+
+arrayLeaders ({16, 17, 4, 3, 5, 2}) ==> return {17, 5, 2}
+Explanation:
+17 is greater than the sum all the elements to its right side
+
+5 is greater than the sum all the elements to its right side
+
+Note : The last element 2 is greater than the sum of its right elements (abstract zero).
+
+arrayLeaders ({5, 2, -1}) ==> return {5, 2}
+Explanation:
+5 is greater than the sum all the elements to its right side
+
+2 is greater than the sum all the elements to its right side
+
+Note : The last element -1 is less than the sum of its right elements (abstract zero).
+
+arrayLeaders ({0, -1, -29, 3, 2}) ==> return {0, -1, 3, 2}
+Explanation:
+0 is greater than the sum all the elements to its right side
+
+-1 is greater than the sum all the elements to its right side
+
+3 is greater than the sum all the elements to its right side
+
+Note : The last element 2 is greater than the sum of its right elements (abstract zero).
+*/
+
+// SOLUTION 1: O(n)
+// arr of num -> arr of leaders
+function arrayLeaders(nums){
+    let leaders = [];
+    let sum = 0;
+    // nums - loop backwards
+    for(let i = nums.length - 1; i >= 0; i--) {
+     // if num > sum, add leaders
+      if(nums[i] > sum) {
+        leaders.push(nums[i]);
+      };
+      sum += nums[i];
+    };
+    
+    return leaders.reverse();
+  };
+  
+  console.log(arrayLeaders([1, 2, 3, 4, 0]), [4]);
+  console.log(arrayLeaders([16, 17, 4, 3, 5, 2]), [17, 5, 2]);
